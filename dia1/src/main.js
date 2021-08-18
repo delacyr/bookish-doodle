dd4080fb7751fda5c2434ee640b940b7cb6ed4cb
delacyr <delacyr@gmail.com>
2021-08-17 20:15:29 -0400

    fixes issue #4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('body').innerHTML = `
+function changeVisibility() {
+  let div = document.querySelector('[data-js="app"]')
+  div.style.display = div.style.display == 'none' ? 'block' : 'none'
+}
+
+document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const button = document.querySelector('[data-js="button"]')
+button.addEventListener('click', () => changeVisibility(), false)
